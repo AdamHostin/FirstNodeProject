9c86505c13c691ad0fb9b89ab37567b55bc81709
adamhostin11 <hostinko@gmail.com>
2023-07-03 16:57:29 +0200

    data models

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose"
+import validator from "validator"
+
+export const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value){
+            if( value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email not valid. Please provide a valid email.')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password cannot include \'password\' substring')
+            }
+        }
+    }
+})
